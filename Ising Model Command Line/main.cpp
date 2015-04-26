595d850e53fd5084b45e33e023966705b55adc67
borundev <borundev@gmail.com>
2015-04-26 11:10:41 -0700

    Initial Commit

---
+++ Ising Model Command Line/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Ising Model Command Line
+//
+//  Created by Borun Chowdhury on 4/26/15.
+//  Copyright (c) 2015 Borun Chowdhury and Mouchumi Bhuyan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
